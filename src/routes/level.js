a3bb5ad8d2ac7a61788175afddc06de13277f90f
jhonperez95 <jhonperez.j95@gmail.com>
2022-01-06 19:43:44 -0500

    code clean

--- src/routes/level.js
+++ src/routes/level.js
@@ -5,7 +5,5 @@ const routesLevels = new Router()
 
 // Add routesLevels
 routesLevels.get('/', ctrLevels.findAllLevels)
-// routesLevels.get('/', (req, res) => {
-//   res.status(200).json({ success: true, message: 'route correct levels' })
-// })
+
 module.exports = routesLevels
